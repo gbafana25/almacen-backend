9c937b35f92203c7710de059188ff0087508c64b
Gareth M. <gkm25@protonmail.com>
2026-06-07 14:06:51 -0500

    added dotenv support for port/address

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,26 @@ use entities::{prelude::*};
 use rocket::*;
 use database::set_up_db;
 use crate::api::{device::{create_device, get_devices}, user::{login, signup, users, validate}, vault::{create_vault, get_all_vaults, get_vault, update_vault}, vault_item::{create_vault_item, get_vault_item, get_vault_items_by_vault, update_vault_item}, vault_key::get_vault_key};
-
+extern crate dotenv;
+use dotenv::dotenv;
+use std::{env, net::Ipv4Addr, str::FromStr};
 
 #[launch] // The "main" function of the program
 async fn rocket() -> _ {
+    dotenv().ok();
+
+    let config = Config {
+        port: env::var("APP_PORT").unwrap_or(String::from("8000")).parse().unwrap_or(8000),
+        address: Ipv4Addr::from_str(&env::var("APP_ADDR").unwrap_or("0.0.0.0".to_string())).unwrap_or(Ipv4Addr::new(0, 0, 0, 0).into()).into(),
+        ..Default::default()
+    };
+
     let db = match set_up_db().await {
        Ok(db) => db,
        Err(err) => panic!("{}", err),
     };
 
-    rocket::build()
+    rocket::custom(config)
         .manage(db)
         .mount("/", routes![users, signup, get_devices, create_device, get_all_vaults, get_vault, create_vault, get_vault_items_by_vault, update_vault, create_vault_item, get_vault_item, update_vault_item, login, get_vault_key, validate])
 }
